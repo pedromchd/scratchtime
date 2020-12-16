0047f407d0474d911812d4641b44851a89942e7a
DarkPixel100 <diegofavila20@gmail.com>
2020-12-16 05:12:23 -0300

    Dropdown completado

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 let body = document.querySelector('body');
 body.addEventListener('click', function(evt) {
   let parent = evt.target.parentElement;
-  console.log(parent);
-  if (evt.target.nodeName === 'DIV' && parent.nodeName === 'MAIN') {
+  // console.log(evt.target);
+  if ((evt.target.nodeName === 'DIV' && parent.nodeName === 'MAIN')) {
     evt.target.classList.toggle('active');
+  } else if (evt.target.nodeName === 'H1' && parent.nodeName === 'DIV') {
+    parent.classList.toggle('active');
   }
 });
