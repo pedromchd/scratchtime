616294cbb04468d177a4f8b6a898960786fe3d74
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2020-12-16 00:01:19 -0300

    ajustando css

--- js/script.js
+++
@@ -1,7 +0,0 @@
-let body = document.querySelector('body');
-body.addEventListener('click', function(evt) {
-  if (evt.target.classList.contains('arrow')) {
-    let div = evt.target.parentElement;
-    div.classList.toggle('active');
-  }
-});
