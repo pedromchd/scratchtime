9372f837306464a70b74fa2d421713374f95cfcd
DarkPixel100 <diegofavila20@gmail.com>
2020-12-18 16:27:11 -0300

    Setas funcionais

--- js/script.js
+++ js/script.js
@@ -2,9 +2,7 @@ let body = document.querySelector('body');
 body.addEventListener('click', function(evt) {
   let parent = evt.target.parentElement;
   // console.log(evt.target);
-  if ((evt.target.nodeName === 'DIV' && parent.nodeName === 'MAIN')) {
-    evt.target.classList.toggle('active');
-  } else if (evt.target.nodeName === 'H1' && parent.nodeName === 'DIV') {
+  if ((evt.target.nodeName === 'IMG' && parent.nodeName === 'DIV')) {
     parent.classList.toggle('active');
   }
 });
